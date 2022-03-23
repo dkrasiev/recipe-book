3e2d88e3ec82dc7d2d058a388c829e61badbdc13
Дмитрий Красиев <dmitrykrasiev@gmail.com>
2022-03-24 01:38:08 +0400

    google sign-in working

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
-  APIkey: 'AIzaSyAfBnHl2h6K5RKLbLBGtPLB3hzuUaFTqDQ'
+  firebase: {
+    projectId: 'recipe-book-62867',
+    appId: '1:655398879797:web:71a7bc1eefbdeb8971e77c',
+    databaseURL: 'https://recipe-book-62867-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'recipe-book-62867.appspot.com',
+    apiKey: 'AIzaSyAfBnHl2h6K5RKLbLBGtPLB3hzuUaFTqDQ',
+    authDomain: 'recipe-book-62867.firebaseapp.com',
+    messagingSenderId: '655398879797',
+    measurementId: 'G-6RNVTHPQ05',
+  }
 };
